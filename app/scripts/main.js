d854571f6e476fb8a04c1dd9f6487d6108bba63a
Eric Motil <emotil@gsandf.com>
2016-12-31 17:26:36 -0600

    dist

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -376,10 +376,10 @@ function onDocumentMouseMove( event ) {
   // define object to watch
   var intersects = raycaster.intersectObject( cubeLink );
   if ( intersects.length > 0 ) {
-    document.body.style.cursor = "pointer";
+    document.body.style.cursor = 'pointer';
     // console.log('intersected');
   } else {
-    document.body.style.cursor = "move";
+    document.body.style.cursor = 'move';
     // console.log('not intersected');
   }
 
@@ -445,29 +445,29 @@ function render() {
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("btn-overlay");
+var btn = document.getElementById('btn-overlay');
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
-    modal.style.display = "block";
+    modal.style.display = 'block';
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+    modal.style.display = 'none';
 }
 
 modal.onclick = function() {
-  modal.style.display = "none";
+  modal.style.display = 'none';
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        modal.style.display = 'none';
     }
 }
 
